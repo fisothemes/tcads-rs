fab5d612f75d73a78bce1a3ee8e81d1d4ffcbd32
Goodwill Mzumala <fisothemes@gmail.com>
2026-01-12 17:30:37 +0000

    feat: add primitive ADS protocol constants to `constants.rs`

--- packages/tcads-core/src/constants.rs
+++ packages/tcads-core/src/constants.rs
@@ -1,1 +1,9 @@
+//! Primitive constants for the ADS protocol.
 
+pub const AMS_TCP_HEADER_LEN: usize = 6;
+pub const AMS_HEADER_LEN: usize = 32;
+
+/// Standard ADS TCP Port
+pub const PORT_AMS_TCP: u16 = 48898;
+/// Standard ADS UDP Port
+pub const PORT_AMS_UDP: u16 = 48899;
