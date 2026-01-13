ca76fe5061a8df7c3c8b101b5b84599bc0dcb260
Goodwill Mzumala <fisothemes@gmail.com>
2026-01-13 07:06:49 +0000

    feat: add `AmsTcpHeader` struct with methods, constants, documentation, and `TryFrom` implementation for TCP header parsing
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-core/src/protocol/header.rs
+++ packages/tcads-core/src/protocol/header.rs
@@ -1,9 +1,13 @@
 use super::commands::CommandId;
 use super::state_flags::StateFlag;
+use crate::constants::{AMS_HEADER_LEN, AMS_TCP_HEADER_LEN};
 use crate::errors::{AdsError, AdsReturnCode};
 use crate::types::addr::AmsAddr;
 use std::io::{self, Write};
 
+/// The 6-byte prefix for TCP communication.
+///
+/// Contains the total length of the AMS packet (AMS Header + ADS Data).
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
 pub struct AmsTcpHeader {
     reserved: u16,
@@ -11,6 +15,9 @@ pub struct AmsTcpHeader {
 }
 
 impl AmsTcpHeader {
+    /// Creates a new TCP header.
+    ///
+    /// `length` is the size of the AMS Packet (AMS Header + ADS Data).
     pub const fn new(length: u32) -> Self {
         Self {
             reserved: 0,
@@ -18,12 +25,25 @@ impl AmsTcpHeader {
         }
     }
 
+    /// Creates a new TCP header with the given reserved and length fields.
     pub const fn with_reserved(reserved: u16, length: u32) -> Self {
         Self { reserved, length }
     }
 
+    /// The reserved field is currently unused and must be set to zero.
+    pub fn reserved(&self) -> u16 {
+        self.reserved
+    }
+
+    /// The size of the AMS Packet (AMS Header + ADS Data).
     pub fn length(&self) -> u32 {
         self.length
+    }
+
+    /// Writes the 6 bytes to a writer.
+    pub fn write_to<W: Write>(&self, w: &mut W) -> io::Result<()> {
+        w.write_all(&self.reserved.to_le_bytes())?;
+        w.write_all(&self.length.to_le_bytes())
     }
 }
 
@@ -31,7 +51,17 @@ impl TryFrom<&[u8]> for AmsTcpHeader {
     type Error = AdsError;
 
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        todo!()
+        if value.len() < AMS_TCP_HEADER_LEN {
+            return Err(AdsError::MalformedPacket("TCP Header data too short"));
+        }
+
+        Ok(
+            // Safely unwraps because length was checked.
+            Self {
+                reserved: u16::from_le_bytes(value[0..2].try_into().unwrap()),
+                length: u32::from_le_bytes(value[2..6].try_into().unwrap()),
+            },
+        )
     }
 }
 
