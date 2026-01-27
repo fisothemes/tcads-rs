4c2225b04083930bef99ee4517878751590dbf4a
Goodwill Mzumala <fisothemes@gmail.com>
2026-01-27 02:20:20 +0000

    feat: begin work on the `tcads-client` crate
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

---
+++ packages/tcads-client/src/client/blocking.rs
@@ -0,0 +1,32 @@
+use std::net::ToSocketAddrs;
+use std::sync::Arc;
+use tcads_core::prelude::AmsNetId;
+
+use crate::errors::Result;
+
+struct InnerClient {}
+
+#[allow(unused)] // TODO: Delete this.
+#[derive(Clone)]
+pub struct Client {
+    inner: Arc<InnerClient>,
+}
+
+impl Client {
+    /// Connects to the local AMS Router (127.0.0.1:48898).
+    ///
+    /// This attempts to perform a "Port Request" handshake to assign a dynamic AMS NetID.
+    pub fn new() -> Result<Self> {
+        Self::connect("127.0.0.1:48898", None)
+    }
+
+    /// Connects to a specific AMS Router.
+    ///
+    /// # Arguments
+    /// * `addr` - The TCP address of the router (e.g. "192.168.0.10:48898").
+    /// * `source_id` - Optional. The NetID to use for this client.
+    ///                 If `None`, the client asks the router to assign one.
+    pub fn connect<A: ToSocketAddrs>(addr: A, source_id: Option<AmsNetId>) -> Result<Self> {
+        todo!()
+    }
+}
---
+++ packages/tcads-client/src/devices/blocking/traits.rs
@@ -0,0 +1,34 @@
+use crate::client::blocking::Client;
+use crate::errors::Result;
+use tcads_core::prelude::{AdsState, AmsAddr};
+use tcads_core::protocol::commands::{AdsDeviceInfoResponse, AdsReadStateResponse};
+
+pub type AdsDeviceInfo = AdsDeviceInfoResponse;
+pub type AdsReadState = AdsReadStateResponse;
+
+/// The core ADS interface.
+///
+/// Implements standard ADS operations (Read, Write, ReadState, Notifications)
+/// exactly as defined in the ADS Specification.
+pub trait AdsDevice {
+    /// Returns a reference to the underlying ADS client connection.
+    fn client(&self) -> &Client;
+
+    /// Returns the target AMS Address of this device.
+    fn addr(&self) -> AmsAddr;
+
+    /// Reads the name and version of the ADS device.
+    fn read_device_info(&self) -> Result<AdsDeviceInfo> {
+        todo!()
+    }
+
+    /// Reads the ADS status and the device status of the ADS device.
+    fn read_state(&self) -> Result<AdsReadState> {
+        todo!()
+    }
+
+    /// Changes the ADS status and the device status of the ADS device.
+    fn write_control(&self, ads_state: AdsState, device_state: u16, data: &[u8]) -> Result<()> {
+        todo!()
+    }
+}
