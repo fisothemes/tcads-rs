37ef4f0716a7529d48d96ecb2fda44bb942137bd
Goodwill Mzumala <fisothemes@gmail.com>
2026-02-09 17:39:36 +0000

    feat: re-export `AMS_PORT_LEN` in `ams` module

--- packages/tcads-core/src/ams/mod.rs
+++ packages/tcads-core/src/ams/mod.rs
@@ -8,4 +8,4 @@ pub use addr::{AMS_ADDR_LEN, AmsAddr, AmsPort};
 pub use command::AmsCommand;
 pub use error::{AddrError, AmsError, AmsTcpHeaderError, NetIdError};
 pub use header::{AMS_TCP_HEADER_LEN, AmsTcpHeader};
-pub use net_id::{AmsNetId, NETID_LEN};
+pub use net_id::{AMS_PORT_LEN, AmsNetId, NETID_LEN};
