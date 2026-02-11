b43a8b84a1470e5fc21b8f982f8ee2fe67d55b44
Goodwill Mzumala <fisothemes@gmail.com>
2026-02-11 04:05:02 +0000

    refactor: move `RouterState` from `protocol/router_notification` to `ams` module
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-core/src/ams/mod.rs
+++ packages/tcads-core/src/ams/mod.rs
@@ -3,9 +3,11 @@ pub mod command;
 pub mod error;
 pub mod header;
 pub mod net_id;
+pub mod router_state;
 
 pub use addr::{AMS_ADDR_LEN, AmsAddr, AmsPort};
 pub use command::AmsCommand;
 pub use error::{AddrError, AmsError, AmsTcpHeaderError, NetIdError};
 pub use header::{AMS_TCP_HEADER_LEN, AmsTcpHeader};
 pub use net_id::{AMS_PORT_LEN, AmsNetId, NETID_LEN};
+pub use router_state::RouterState;
