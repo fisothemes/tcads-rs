9270ff5d760b6c50b00c824426856a3dc27cc17c
Goodwill Mzumala <fisothemes@gmail.com>
2026-02-13 14:34:05 +0000

    refactor: expand `pub use` exports with specialised error types in `ads` module

--- packages/tcads-core/src/ads/mod.rs
+++ packages/tcads-core/src/ads/mod.rs
@@ -5,7 +5,7 @@ pub mod return_codes;
 pub mod state_flag;
 
 pub use command::AdsCommand;
-pub use error::AdsError;
+pub use error::{AdsCommandError, AdsError, AdsHeaderError, AdsReturnCodeError, StateFlagError};
 pub use header::AdsHeader;
 pub use return_codes::AdsReturnCode;
 pub use state_flag::StateFlag;
