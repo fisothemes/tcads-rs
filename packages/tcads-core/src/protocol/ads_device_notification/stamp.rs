4c8a03b349e1182bc0f1ac6cec2f8f8868e21c25
Goodwill Mzumala <fisothemes@gmail.com>
2026-02-22 02:36:08 +0000

    feat: add `AdsStampHeader` and `AdsStampHeaderOwned` structs with no impls to `ads_device_notification::stamp` module
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-core/src/protocol/ads_device_notification/stamp.rs
+++ packages/tcads-core/src/protocol/ads_device_notification/stamp.rs
@@ -1,1 +1,14 @@
+use super::sample::{AdsNotificationSample, AdsNotificationSampleOwned};
+use crate::ads::WindowsFileTime;
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct AdsStampHeader<'a> {
+    timestamp: WindowsFileTime,
+    samples: Vec<AdsNotificationSample<'a>>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct AdsStampHeaderOwned {
+    timestamp: WindowsFileTime,
+    samples: Vec<AdsNotificationSampleOwned>,
+}
