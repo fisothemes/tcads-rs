e6b066a056a3e7b0124234e63e5c64d3dc509a59
Goodwill Mzumala <fisothemes@gmail.com>
2026-02-23 16:07:26 +0000

    docs: add crate-level documentation for `tcads` library

--- packages/tcads/src/lib.rs
+++ packages/tcads/src/lib.rs
@@ -1,3 +1,17 @@
+//! # TwinCAT ADS for Rust
+//!
+//! A rust-native implementation of the TwinCAT ADS protocol.
+//!
+//! This crate aims to provide a robust way to communicate with TwinCAT devices (PLCs, NC, etc.),
+//! without relying on the official Beckhoff `TcAdsDll.dll` or requiring a local TwinCAT
+//! installation on the client machine.
+//!
+//! This crate is composed of the following sub-crates:
+//!
+//! - [`core`] - Protocol primitives, serialization, and frame I/O
+//! - [`client`] - High-level connection and request management for ADS devices.
+//! - [`server`] - Framework for building custom ADS servers/devices in Rust.
+
 pub use tcads_client as client;
 pub use tcads_core as core;
 pub use tcads_server as server;
