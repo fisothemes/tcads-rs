0fb38609c5977a01b63feaa2803de44a0fdc1f4c
Goodwill Mzumala <fisothemes@gmail.com>
2026-03-06 03:43:25 +0000

    feat: add `PoisonedLock` variant and `From<PoisonError>` implementation to tcads-client `Error` enum
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-client/src/error.rs
+++ packages/tcads-client/src/error.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::sync::PoisonError;
 use tcads_core::ads::AdsReturnCode;
 use tcads_core::protocol::ProtocolError;
 
@@ -12,6 +13,14 @@ pub enum Error {
     AdsReturnCode(#[from] AdsReturnCode),
     #[error("Disconnected")]
     Disconnected,
+    #[error("Poisoned Lock: {0}")]
+    PoisonedLock(String),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+impl<T> From<PoisonError<T>> for Error {
+    fn from(err: PoisonError<T>) -> Self {
+        Error::PoisonedLock(err.to_string())
+    }
+}
