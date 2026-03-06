dc1b8b2d3fe31ee631cb6d87ad37d53adc90fcef
Goodwill Mzumala <fisothemes@gmail.com>
2026-03-06 04:41:01 +0000

    feat: improve `Error::Io` message and add tests for `Error` enum conversions
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-client/src/error.rs
+++ packages/tcads-client/src/error.rs
@@ -5,7 +5,7 @@ use tcads_core::protocol::ProtocolError;
 
 #[derive(Debug, Clone, thiserror::Error)]
 pub enum Error {
-    #[error(transparent)]
+    #[error("I/O error: {0}")]
     Io(Arc<io::Error>),
     #[error(transparent)]
     Protocol(#[from] ProtocolError),
@@ -28,5 +28,30 @@ impl<T> From<PoisonError<T>> for Error {
 impl From<io::Error> for Error {
     fn from(err: io::Error) -> Self {
         Error::Io(Arc::new(err))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Mutex;
+
+    #[test]
+    fn io_error_converts_and_clones() {
+        let err = Error::from(io::Error::new(io::ErrorKind::ConnectionRefused, "refused"));
+        let cloned = err.clone();
+        assert!(matches!(cloned, Error::Io(_)));
+        assert!(err.to_string().contains("refused"));
+    }
+
+    #[test]
+    fn poison_error_converts() {
+        let mutex = Mutex::new(0u32);
+        let _ = std::panic::catch_unwind(|| {
+            let _guard = mutex.lock().unwrap();
+            panic!("poison the lock");
+        });
+        let err = Error::from(mutex.lock().unwrap_err());
+        assert!(matches!(err, Error::PoisonedLock(_)));
     }
 }
