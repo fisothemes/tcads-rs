594f42eeda351f9e87a9c2a034fd86f27f52eac4
Goodwill Mzumala <fisothemes@gmail.com>
2026-03-09 04:19:53 +0000

    feat: add `RouterNotificationDispatcher` for broadcasting router state changes to subscribers
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-client/src/blocking/task/mod.rs
+++ packages/tcads-client/src/blocking/task/mod.rs
@@ -2,6 +2,9 @@ pub mod dispatcher;
 pub mod reader;
 pub mod writer;
 
-pub use dispatcher::{AdsNotificationDispatcher, AmsRequestDispatchKey, AmsRequestDispatcher};
+pub use dispatcher::{
+    AdsNotificationDispatcher, AmsRequestDispatchKey, AmsRequestDispatcher,
+    RouterNotificationDispatcher,
+};
 pub use reader::AmsResponseReader;
 pub use writer::AmsRequestWriter;
