1403a3cef7ed5f0af7d351da7c3b159aa2791313
Goodwill Mzumala <fisothemes@gmail.com>
2026-03-10 04:52:15 +0000

    feat: expand `tcads-core` re-exports with additional ADS and AMS types, including `AdsState`, `AdsTransMode`, and `ProtocolError`
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-client/src/lib.rs
+++ packages/tcads-client/src/lib.rs
@@ -3,8 +3,9 @@ pub mod error;
 pub mod tokio;
 
 pub use tcads_core::{
-    ads::{AdsReturnCode, IndexGroup, IndexOffset},
-    ams::{AmsAddr, AmsNetId, AmsPort},
+    ads::{AdsReturnCode, AdsState, AdsTransMode, IndexGroup, IndexOffset, InvokeId},
+    ams::{AmsAddr, AmsNetId, AmsPort, RouterState},
+    protocol::{AdsNotificationSampleOwned, ProtocolError},
 };
 
 pub use error::{Error, Result};
