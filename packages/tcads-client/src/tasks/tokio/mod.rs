95fcc38cfba6a052ea86c6ded243e77a0f7f45b0
Goodwill Mzumala <fisothemes@gmail.com>
2026-03-16 01:01:20 +0000

    feat: re-export `AmsResponseReader` in `tokio` tasks module in `tcads-client`
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-client/src/tasks/tokio/mod.rs
+++ packages/tcads-client/src/tasks/tokio/mod.rs
@@ -6,4 +6,5 @@ pub use super::AmsRequestDispatchKey;
 pub use dispatcher::{
     AdsNotificationDispatcher, AmsRequestDispatcher, RouterNotificationDispatcher,
 };
+pub use reader::AmsResponseReader;
 pub use writer::AmsRequestWriter;
