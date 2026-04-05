3b69e578fd1085ec1cf142597a04addb29541261
Goodwill Mzumala <fisothemes@gmail.com>
2026-04-05 23:04:18 +0100

    refactor: rename `byte_size` fields to `blob_size` in `AdsSymbolUploadInfo` for clarity
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-core/src/ads/symbol/upload_info.rs
+++ packages/tcads-core/src/ads/symbol/upload_info.rs
@@ -7,20 +7,20 @@ use super::error::AdsSymbolUploadInfoError;
 ///
 /// # Wire Format
 ///
-/// | Offset | Size | Field                    | Description                                    |
-/// |--------|------|--------------------------|------------------------------------------------|
-/// | 0      | 4    | `symbol_count`           | Number of symbols                              |
-/// | 4      | 4    | `symbol_byte_size`       | Total byte size of the symbol blob             |
-/// | 8      | 4    | `data_type_count`        | Number of data types                           |
-/// | 12     | 4    | `data_type_byte_size`    | Total byte size of the data type blob          |
-/// | 16     | 4    | `dyn_symbol_capacity`    | Maximum number of dynamic symbols              |
-/// | 20     | 4    | `dyn_symbol_count`       | Number of dynamic symbols currently in use     |
+/// | Offset | Size | Field                 | Description                                |
+/// |--------|------|-----------------------|--------------------------------------------|
+/// | 0      | 4    | `symbol_count`        | Number of symbols                          |
+/// | 4      | 4    | `symbol_blob_size`    | Total byte size of the symbol blob         |
+/// | 8      | 4    | `data_type_count`     | Number of data types                       |
+/// | 12     | 4    | `data_type_blob_size` | Total byte size of the data type blob      |
+/// | 16     | 4    | `dyn_symbol_capacity` | Maximum number of dynamic symbols          |
+/// | 20     | 4    | `dyn_symbol_count`    | Number of dynamic symbols currently in use |
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
 pub struct AdsSymbolUploadInfo {
     symbol_count: u32,
-    symbol_byte_size: u32,
+    symbol_blob_size: u32,
     data_type_count: u32,
-    data_type_byte_size: u32,
+    data_type_blob_size: u32,
     dyn_symbol_capacity: u32,
     dyn_symbol_count: u32,
 }
@@ -32,17 +32,17 @@ impl AdsSymbolUploadInfo {
     /// Creates a new instance of [`AdsSymbolUploadInfo`].
     pub fn new(
         symbol_count: u32,
-        symbol_byte_size: u32,
+        symbol_blob_size: u32,
         data_type_count: u32,
-        data_type_byte_size: u32,
+        data_type_blob_size: u32,
         dyn_symbol_capacity: u32,
         dyn_symbol_count: u32,
     ) -> Self {
         Self {
             symbol_count,
-            symbol_byte_size,
+            symbol_blob_size,
             data_type_count,
-            data_type_byte_size,
+            data_type_blob_size,
             dyn_symbol_capacity,
             dyn_symbol_count,
         }
@@ -56,8 +56,8 @@ impl AdsSymbolUploadInfo {
     /// Total byte size of the symbol blob (`ADSIGRP_SYM_UPLOAD`, `0xF00B`).
     ///
     /// Use this as the read length when fetching the full symbol blob.
-    pub fn symbol_byte_size(&self) -> u32 {
-        self.symbol_byte_size
+    pub fn symbol_blob_size(&self) -> u32 {
+        self.symbol_blob_size
     }
 
     /// Number of data types in the data type blob.
@@ -68,8 +68,8 @@ impl AdsSymbolUploadInfo {
     /// Total byte size of the data type blob (`ADSIGRP_SYM_DT_UPLOAD`, `0xF00E`).
     ///
     /// Use this as the read length when fetching the full data type blob.
-    pub fn data_type_byte_size(&self) -> u32 {
-        self.data_type_byte_size
+    pub fn data_type_blob_size(&self) -> u32 {
+        self.data_type_blob_size
     }
 
     /// Maximum number of dynamic symbols supported by this runtime.
@@ -86,9 +86,9 @@ impl AdsSymbolUploadInfo {
     pub fn to_bytes(&self) -> [u8; Self::LENGTH] {
         let mut buf = [0u8; Self::LENGTH];
         buf[0..4].copy_from_slice(&self.symbol_count.to_le_bytes());
-        buf[4..8].copy_from_slice(&self.symbol_byte_size.to_le_bytes());
+        buf[4..8].copy_from_slice(&self.symbol_blob_size.to_le_bytes());
         buf[8..12].copy_from_slice(&self.data_type_count.to_le_bytes());
-        buf[12..16].copy_from_slice(&self.data_type_byte_size.to_le_bytes());
+        buf[12..16].copy_from_slice(&self.data_type_blob_size.to_le_bytes());
         buf[16..20].copy_from_slice(&self.dyn_symbol_capacity.to_le_bytes());
         buf[20..24].copy_from_slice(&self.dyn_symbol_count.to_le_bytes());
         buf
@@ -104,9 +104,9 @@ impl AdsSymbolUploadInfo {
         }
         Ok(Self {
             symbol_count: u32::from_le_bytes(data[0..4].try_into().unwrap()),
-            symbol_byte_size: u32::from_le_bytes(data[4..8].try_into().unwrap()),
+            symbol_blob_size: u32::from_le_bytes(data[4..8].try_into().unwrap()),
             data_type_count: u32::from_le_bytes(data[8..12].try_into().unwrap()),
-            data_type_byte_size: u32::from_le_bytes(data[12..16].try_into().unwrap()),
+            data_type_blob_size: u32::from_le_bytes(data[12..16].try_into().unwrap()),
             dyn_symbol_capacity: u32::from_le_bytes(data[16..20].try_into().unwrap()),
             dyn_symbol_count: u32::from_le_bytes(data[20..24].try_into().unwrap()),
         })
@@ -152,9 +152,9 @@ mod tests {
     fn parses_real_capture() {
         let info = AdsSymbolUploadInfo::try_from_slice(&real_bytes()).unwrap();
         assert_eq!(info.symbol_count(), 145);
-        assert_eq!(info.symbol_byte_size(), 15712);
+        assert_eq!(info.symbol_blob_size(), 15712);
         assert_eq!(info.data_type_count(), 57);
-        assert_eq!(info.data_type_byte_size(), 11296);
+        assert_eq!(info.data_type_blob_size(), 11296);
         assert_eq!(info.dyn_symbol_capacity(), 2000);
         assert_eq!(info.dyn_symbol_count(), 0);
     }
