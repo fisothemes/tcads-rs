c3158ab4d69425ea53594eba67152ee789aac074
Goodwill Mzumala <fisothemes@gmail.com>
2026-06-18 07:59:40 +0100

    feat: begin work on the `RuntimeDevice` in `tcads-client`
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-client/src/devices/mod.rs
+++ packages/tcads-client/src/devices/mod.rs
@@ -1,11 +1,13 @@
 pub mod ads_device;
 pub mod logger;
+pub mod runtime;
 pub mod symbol_info;
 pub mod type_info;
 
 pub mod blocking {
     pub use super::ads_device::blocking::AdsDevice;
     pub use super::logger::blocking::{LogEntryReceiver, Logger};
+    pub use super::runtime::blocking::RuntimeDevice;
     pub use super::symbol_info::blocking::SymbolInfoDevice;
     pub use super::type_info::blocking::TypeInfoDevice;
 }
@@ -13,6 +15,7 @@ pub mod blocking {
 pub mod tokio {
     pub use super::ads_device::tokio::AdsDevice;
     pub use super::logger::tokio::{LogEntryReceiver, Logger};
+    pub use super::runtime::tokio::RuntimeDevice;
     pub use super::symbol_info::tokio::SymbolInfoDevice;
     pub use super::type_info::tokio::TypeDevice;
 }
