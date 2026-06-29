7407f1582fac0907bf3c4018d24f52ef58ddbc8b
Goodwill Mzumala <fisothemes@gmail.com>
2026-06-29 04:37:21 +0100

    feat: add `get_platform_ptr_size` method to `TypeProvider` trait for determining platform pointer size
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-serde/src/type_provider.rs
+++ packages/tcads-serde/src/type_provider.rs
@@ -16,4 +16,9 @@ pub trait TypeProvider {
     /// This method is called frequently during serialization and deserialization of nested structs.
     /// The provider should return `O(1)` lookups.
     fn get_type_info(&self, type_name: &str) -> Option<&AdsTypeInfo>;
+
+    /// Returns the platform pointer size.
+    ///
+    /// This can be used to correctly interpret between interfaces vs FBs in some situations.
+    fn get_platform_ptr_size(&self) -> u8;
 }
