ef6ba6e00d04f7c99f92bccd808c3d15f00fb840
Goodwill Mzumala <fisothemes@gmail.com>
2026-06-29 05:45:57 +0100

    feat: add `from_bytes` function for deserializing byte buffers using PLC type metadata
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-serde/src/de/mod.rs
+++ packages/tcads-serde/src/de/mod.rs
@@ -1,1 +1,16 @@
 pub mod deserializer;
+
+use crate::TypeProvider;
+pub use deserializer::AdsDeserializer;
+use serde::de::DeserializeOwned;
+use tcads_core::AdsTypeInfo;
+
+/// Deserializes a byte buffer into a Rust type, using the given PLC type metadata.
+pub fn from_bytes<T, P>(data: &[u8], type_info: &AdsTypeInfo, provider: &P) -> crate::Result<T>
+where
+    T: DeserializeOwned,
+    P: TypeProvider,
+{
+    let deserializer = AdsDeserializer::new(data, type_info, provider);
+    T::deserialize(deserializer)
+}
