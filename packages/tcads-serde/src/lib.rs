245e3afaea089a990ff4a41b23e66809e142400b
Goodwill Mzumala <fisothemes@gmail.com>
2026-07-07 02:28:11 +0100

    feat: re-export `from_bytes` and `AdsDeserializer` from `de` module
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-serde/src/lib.rs
+++ packages/tcads-serde/src/lib.rs
@@ -4,6 +4,7 @@ pub mod ser;
 pub mod type_provider;
 pub mod value;
 
+pub use de::{AdsDeserializer, from_bytes};
 pub use error::{Error, Result};
 pub use type_provider::TypeProvider;
 pub use value::{Float, Integer, Number, SignedInteger, UnsignedInteger, Value};
