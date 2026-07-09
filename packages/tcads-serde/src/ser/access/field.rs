7cf1cd0695057a4d6e68d2b8473d96c232ef804f
Goodwill Mzumala <fisothemes@gmail.com>
2026-07-09 17:54:31 +0100

    feat: introduce `write_field_bytes` for field serialization in `tcads-serde::ser::access`

---
+++ packages/tcads-serde/src/ser/access/field.rs
@@ -0,0 +1,34 @@
+use crate::TypeProvider;
+use crate::resolvers::resolve_alias;
+use crate::ser::AdsSerializer;
+use tcads_core::AdsFieldInfo;
+
+pub fn write_field_bytes<T, P>(
+    output: &mut [u8],
+    field: &AdsFieldInfo,
+    provider: &P,
+    value: &T,
+) -> Result<(), crate::Error>
+where
+    T: ?Sized + serde::Serialize,
+    P: TypeProvider,
+{
+    let start = field.offset() as usize;
+    let end = start + field.size() as usize;
+    let len = output.len();
+    let field_output = output
+        .get_mut(start..end)
+        .ok_or(crate::Error::SizeMismatch {
+            expected: end,
+            got: len,
+        })?;
+
+    let field_type_name = field.type_name();
+    let ptr_size = provider.get_platform_ptr_size();
+    let raw_type_info = provider
+        .get_type_info(field_type_name)
+        .ok_or_else(|| crate::Error::TypeNotFound(field_type_name.to_string()))?;
+    let field_type_info = resolve_alias(raw_type_info, provider, ptr_size)?;
+
+    value.serialize(AdsSerializer::new(field_output, field_type_info, provider))
+}
--- packages/tcads-serde/src/ser/access/mod.rs
+++ packages/tcads-serde/src/ser/access/mod.rs
@@ -1,3 +1,5 @@
 pub mod array_serializer;
+pub mod field;
 
 pub use array_serializer::AdsArraySerializer;
+pub use field::write_field_bytes;
