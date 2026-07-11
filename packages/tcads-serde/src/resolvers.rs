32061050d75b895eaed899c19b03c1ffe65d7919
Goodwill Mzumala <fisothemes@gmail.com>
2026-07-11 19:21:24 +0100

    refactor: add alias resolution depth limit to prevent infinite loops in `tcads-serde::resolvers`
    Signed-off-by: Goodwill Mzumala <fisothemes@gmail.com>

--- packages/tcads-serde/src/resolvers.rs
+++ packages/tcads-serde/src/resolvers.rs
@@ -1,16 +1,24 @@
 use crate::TypeProvider;
 use tcads_core::{AdsTypeCategory, AdsTypeInfo};
 
+const MAX_ALIAS_DEPTH: usize = 32;
+
 pub fn resolve_alias<'a>(
     type_info: &'a AdsTypeInfo,
     provider: &'a impl TypeProvider,
     platform_ptr_size: u8,
 ) -> Result<&'a AdsTypeInfo, crate::Error> {
     let mut type_info = type_info;
-    while AdsTypeCategory::determine(type_info, platform_ptr_size) == AdsTypeCategory::Alias {
+    for _ in 0..MAX_ALIAS_DEPTH {
+        if AdsTypeCategory::determine(type_info, platform_ptr_size) != AdsTypeCategory::Alias {
+            return Ok(type_info);
+        }
         type_info = provider
             .get_type_info(type_info.type_name())
             .ok_or_else(|| crate::Error::TypeNotFound(type_info.type_name().to_string()))?;
     }
-    Ok(type_info)
+    Err(crate::Error::Custom(format!(
+        "alias chain exceeds {MAX_ALIAS_DEPTH} levels resolving '{}': type table likely contains a cycle",
+        type_info.name(),
+    )))
 }
