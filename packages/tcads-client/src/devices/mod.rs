50eaad48680cdcc6f01de64dc45451b51ce5b5ad
Goodwill Mzumala <fisothemes@gmail.com>
2026-07-16 01:14:35 +0100

    feat: expose `SymbolVersionReceiver` in blocking and async runtimes

--- packages/tcads-client/src/devices/mod.rs
+++ packages/tcads-client/src/devices/mod.rs
@@ -5,11 +5,11 @@ pub mod runtime;
 pub mod blocking {
     pub use super::ads_device::blocking::AdsDevice;
     pub use super::logger::blocking::{LogEntryReceiver, Logger};
-    pub use super::runtime::blocking::RuntimeDevice;
+    pub use super::runtime::blocking::{RuntimeDevice, SymbolVersionReceiver};
 }
 
 pub mod tokio {
     pub use super::ads_device::tokio::AdsDevice;
     pub use super::logger::tokio::{LogEntryReceiver, Logger};
-    pub use super::runtime::tokio::RuntimeDevice;
+    pub use super::runtime::tokio::{RuntimeDevice, SymbolVersionReceiver};
 }
